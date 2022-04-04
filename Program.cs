18a4fadc80acaeac404ef83fda3d548b92b679fe
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-04 15:43:50 +0100

    created board class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter number of rows");
+           
+            Console.WriteLine("Please enter number of columns");
+           
+            int numberOfbombs;
+            string bomb = "x";
+            Console.WriteLine(board);
+
         }
+
     }
 }
+
+
