242faa85d8291f0cebe8a58c9658ef7f81babab3
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-04 17:28:04 +0100

    attept to create grid

--- Program.cs
+++ Program.cs
@@ -6,14 +6,12 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter number of rows");
-           
-            Console.WriteLine("Please enter number of columns");
             Board board = new Board(0,0);
             Console.WriteLine("Please enter the number of rows");
             board.setx(int.Parse(Console.ReadLine()));
             Console.WriteLine("Please enter the number of columns");
             board.sety(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
             int numberOfbombs;
             string bomb = "x";
 
