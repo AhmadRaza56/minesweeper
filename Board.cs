bdb03355c9d55e23d8549c9b9752d5372252710d
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-04 16:25:25 +0100

    created set and gets for rows and columns

--- Board.cs
+++ Board.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 
-public class Board
+internal class Board
 {
-	public Board()
+	int rows;
+	int columns;	
+	public Board(int x, int y)
 	{
-		int rows;
-		int columns;
+		this.rows = x;
+		this.columns = y;
+		int[,] board = new int[x, y];
+	}
+
+	public int getx()
+    {
+		return this.rows;
+    }
+	public void setx(int x)
+    {
+		this.rows = x;
+    }
+	public int gety()
+	{
+		return this.columns;
+	}
+	public void sety(int y)
+	{
+		this.columns = y;
 	}
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,13 @@ namespace minesweeper
             Console.WriteLine("Please enter number of rows");
            
             Console.WriteLine("Please enter number of columns");
-           
+            Board board = new Board(0,0);
+            Console.WriteLine("Please enter the number of rows");
+            board.setx(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Please enter the number of columns");
+            board.sety(int.Parse(Console.ReadLine()));
             int numberOfbombs;
             string bomb = "x";
-            Console.WriteLine(board);
 
         }
 
