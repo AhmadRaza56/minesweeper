a4e0c5158981d5225fe2362fdab90a3ac0a1a30a
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-05 16:16:56 +0100

    three modes over user input to grid size

--- Program.cs
+++ Program.cs
@@ -7,28 +7,78 @@ namespace minesweeper
         static void Main(string[] args)
         {
 			//creating grid size
-			 Console.WriteLine("Please enter number of rows");
-			 int Horizontal = Int32.Parse(Console.ReadLine()); 
-			 Console.WriteLine("Please enter number of columns");
-			 int Vertical = Int32.Parse(Console.ReadLine());
-			 int[][] grid = new int[Vertical][];
+			Console.WriteLine("Please choose the difficulty. Easy, Medium or Hard");
+			string size = Console.ReadLine(); 
+			if (size.ToLower() == "easy")
+            {
+				int horizontal = 10;
+				int vertical = 10;
+				int[][] grid = new int[vertical][];
 
-			 for (int x = 0; x < grid.Length; ++x)
-			 {				
-				grid[x] = new int[Horizontal];
-			 }
-
-			 for (int x = 0; x < grid.Length; ++x)
-			 {
-				//run a nested loop
-				for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
+				for (int x = 0; x < grid.Length; ++x)
 				{
-					grid[x][y] = 0;
-					Console.Write(grid[x][y] + " ");
+					grid[x] = new int[horizontal];
 				}
-				Console.WriteLine();
-			 }
-			//end grid cretions size
+
+				for (int x = 0; x < grid.Length; ++x)
+				{
+					//run a nested loop
+					for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
+					{
+						grid[x][y] = 0;
+						Console.Write(grid[x][y] + " ");
+					}
+					Console.WriteLine();
+				}
+			}
+			else if (size.ToLower() == "medium")
+            {
+				int horizontal = 16;
+				int vertical = 16;
+				int[][] grid = new int[vertical][];
+
+				for (int x = 0; x < grid.Length; ++x)
+				{
+					grid[x] = new int[horizontal];
+				}
+
+				for (int x = 0; x < grid.Length; ++x)
+				{
+					//run a nested loop
+					for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
+					{
+						grid[x][y] = 0;
+						Console.Write(grid[x][y] + " ");
+					}
+					Console.WriteLine();
+				}
+			}
+			else if (size.ToLower() == "hard")
+            {
+				int horizontal = 20;
+				int vertical = 20;
+				int[][] grid = new int[vertical][];
+
+				for (int x = 0; x < grid.Length; ++x)
+				{
+					grid[x] = new int[horizontal];
+				}
+
+				for (int x = 0; x < grid.Length; ++x)
+				{
+					//run a nested loop
+					for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
+					{
+						grid[x][y] = 0;
+						Console.Write(grid[x][y] + " ");
+					}
+					Console.WriteLine();
+				}
+			}			 
+			 //end grid cretions size
+
+			 //start mines
+
 		}
 
 	}
