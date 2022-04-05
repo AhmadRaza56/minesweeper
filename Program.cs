8cb2a2ba70e2ccf49d8ce12f2a7df9638a96a342
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-05 09:58:43 +0100

    customisable grid size

--- Program.cs
+++ Program.cs
@@ -6,30 +6,27 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-			const int Horizontal = 5;
-			const int Vertical = 3;
+			 Console.WriteLine("Please enter number of rows");
+			 int Horizontal = Int32.Parse(Console.ReadLine()); 
+			 Console.WriteLine("Please enter number of columns");
+			 int Vertical = Int32.Parse(Console.ReadLine());
+			 int[][] grid = new int[Vertical][];
 
+			 for (int x = 0; x < grid.Length; ++x)
+			 {				
+				grid[x] = new int[Horizontal];
+			 }
 
-			int[][] grid = new int[Vertical][];//this will have 10 arrays inside
-			for (int x = 0; x < grid.Length; ++x)
-			{
-				//initialise each sub array
-				grid[x] = new int[Horizontal];//the inner array will have 6 elements
-			}
-
-			Random rand = new Random();
-
-			//fill the grid with random values
-			for (int x = 0; x < grid.Length; ++x)
-			{
+			 for (int x = 0; x < grid.Length; ++x)
+			 {
 				//run a nested loop
 				for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
 				{
-					grid[x][y] = rand.Next(10);//random value between 0 and 10 (inclusive)
+					grid[x][y] = 0;
 					Console.Write(grid[x][y] + " ");
 				}
 				Console.WriteLine();
-			}
+			 }
 
 		}
 
