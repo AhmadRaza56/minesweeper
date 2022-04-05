1844e050ea7d0c6f89da207352fba52d2765f8e4
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-05 09:31:35 +0100

    create grid

--- Program.cs
+++ Program.cs
@@ -6,16 +6,32 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-            Board board = new Board(0,0);
-            Console.WriteLine("Please enter the number of rows");
-            board.setx(int.Parse(Console.ReadLine()));
-            Console.WriteLine("Please enter the number of columns");
-            board.sety(int.Parse(Console.ReadLine()));
-            Console.WriteLine();
-            int numberOfbombs;
-            string bomb = "x";
+			const int Horizontal = 5;
+			const int Vertical = 3;
 
-        }
+
+			int[][] grid = new int[Vertical][];//this will have 10 arrays inside
+			for (int x = 0; x < grid.Length; ++x)
+			{
+				//initialise each sub array
+				grid[x] = new int[Horizontal];//the inner array will have 6 elements
+			}
+
+			Random rand = new Random();
+
+			//fill the grid with random values
+			for (int x = 0; x < grid.Length; ++x)
+			{
+				//run a nested loop
+				for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
+				{
+					grid[x][y] = rand.Next(10);//random value between 0 and 10 (inclusive)
+					Console.Write(grid[x][y] + " ");
+				}
+				Console.WriteLine();
+			}
+
+		}
 
     }
 }
