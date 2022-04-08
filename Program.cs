605d89136486bc292b2b4ffb46d24c6e53f26c63
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-08 15:40:44 +0100

    fix namespace

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 
-namespace minesweeper
+namespace MineSweeper
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Board board = newe 
 			Console.WriteLine("Welcome to Minesweeper!");
 			//creating grid size
 			Console.WriteLine("Please choose the difficulty. Easy, Medium or Hard");
-			string size = Console.ReadLine();
+			Boa Console.ReadLine();
 		}
 	}
 }
