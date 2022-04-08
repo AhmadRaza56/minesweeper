4629693cf745964a1e9d770466d175fdd5297b81
Ahmad Raza <razaahmad1998@gmail.com>
2022-04-08 14:43:28 +0100

    tidy up program.cs

--- Program.cs
+++ Program.cs
@@ -10,60 +10,6 @@ namespace minesweeper
 			//creating grid size
 			Console.WriteLine("Please choose the difficulty. Easy, Medium or Hard");
 			string size = Console.ReadLine();
-
-			
-			if (size.ToLower() == "easy")
-			{
-				
-
-			}
-			else if (size.ToLower() == "medium")
-			{
-				int horizontal = 16;
-				int vertical = 16;
-				int[][] grid = new int[vertical][];
-
-				for (int x = 0; x < grid.Length; ++x)
-				{
-					grid[x] = new int[horizontal];
-				}
-
-				for (int x = 0; x < grid.Length; ++x)
-				{
-					//run a nested loop
-					for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
-					{
-						grid[x][y] = 0;
-						Console.Write(grid[x][y] + " ");
-					}
-					Console.WriteLine();
-				}
-			}
-			else if (size.ToLower() == "hard")
-			{
-				int horizontal = 20;
-				int vertical = 20;
-				int[][] grid = new int[vertical][];
-
-				for (int x = 0; x < grid.Length; ++x)
-				{
-					grid[x] = new int[horizontal];
-				}
-
-				for (int x = 0; x < grid.Length; ++x)
-				{
-					//run a nested loop
-					for (int y = 0; y < grid[x].Length; ++y)//loop through an inner array
-					{
-						grid[x][y] = 0;
-						Console.Write(grid[x][y] + " ");
-					}
-					Console.WriteLine();
-				}
-			}
-			//end grid cretions size
-				
-
 		}
 	}
 }
